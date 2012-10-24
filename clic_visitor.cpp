d4911c3840e93432663576909fcc1714c55fc3d3
Tom Sun <tomjpsun@gmail.com>
2012-10-24 17:17:20 +0800

    make the searched symbol as the parameter

--- clic_visitor.cpp
+++ clic_visitor.cpp
@@ -44,21 +44,22 @@ enum CXChildVisitResult visitor_f(
         unsigned int line, column, offset;
 	MatchPattern* mp = (MatchPattern *)clientData;
 
-        // if corresponding file is invalid,
-	// visit sibling
+        // if corresponding file is invalid, then visit sibling
 	clang_getInstantiationLocation(
                 clang_getCursorLocation(cursor),
                 &file, &line, &column, &offset);
 
         if (!clang_getFileName(file).data) {
-		printf("visitor(): no file data, visit sibling\n");
+		//printf("visitor(): no file data, visit sibling\n");
 		return CXChildVisit_Continue;
 	}
 
+	// print if the cursor matches the clientData
 	const char* info = clang_getCString(clang_getCursorSpelling(cursor));
 	if ((strlen(info) > 0) && (strcmp(mp->name, info) == 0)) {
 		const char* kind = clang_getCString(clang_getCursorKindSpelling(clang_getCursorKind(cursor)));
-		printf("(%d, %d) %s\t: %s\n", line, column, kind, info);
+		const char* fname = clang_getCString(clang_getFileName(file));
+		printf("%s: (%d, %d) %s\t: %s\n",fname, line, column, kind, info);
 	}
         return CXChildVisit_Recurse;
 }
@@ -66,7 +67,7 @@ enum CXChildVisitResult visitor_f(
 int main (int argc, char* argv[])
 {
 	if (argc < 2) {
-		fprintf(stderr, "Usage:\n\t <-s ast_file>[,ast_file]...\n");
+		fprintf(stderr, "Usage:\n\t <-s symbol> <-f file1>[,file2]...\n");
 		return 1;
 	}
 
@@ -75,15 +76,24 @@ int main (int argc, char* argv[])
 	char* p;
 	char* tmp_files;
 	CXTranslationUnit tu_array[64];
+	char* symbol;
 
-	while ((c = getopt(argc, argv, "s:")) != -1) {
+	while ((c = getopt(argc, argv, "s:f:")) != -1) {
 		switch (c) {
 		case 's':
+			symbol = optarg;
+			break;
+		case 'f':
 			tmp_files = optarg;
 			break;
 		default:
 			printf("unknow option -%c.\n", optopt);
 		}
+	}
+
+	if (symbol == NULL) {
+		fprintf(stderr, "No symbol specified, exit.\n");
+		return 1;
 	}
 
 	p = strtok(tmp_files, ",");
@@ -94,17 +104,29 @@ int main (int argc, char* argv[])
 	}
 
         // debug print
+	printf("query symbol '%s'\n", symbol);
 	printf("total %d ast files:\n", fcnt);
 	for (int i=0; i<fcnt; i++)
 	        printf("\t%s\n", ast_files[i]);
 
 	CXIndex cidx = clang_createIndex(0, 0);
 	MatchPattern m;
-	init_match_pattern(&m, MATCH_DEFINE, "dump");
+	init_match_pattern(&m, MATCH_DEFINE, symbol);
+
+	const char* command_line_args[3] = {
+		"-I/usr/lib/gcc/i486-linux-gnu/4.7/include",
+		"-I/usr/GNUstep/Local/Library",
+		"-I/usr/GNUstep/System/Library/Makefiles/TestFramework"
+	};
 
 	for(int i=0; i<fcnt; i++) {
-		tu_array[i] = clang_createTranslationUnit(cidx, ast_files[i]);
-		CXCursor current_tu_cursor = clang_getTranslationUnitCursor(tu_array[i]);
-		clang_visitChildren(current_tu_cursor, visitor_f, &m);
+		tu_array[i] = clang_parseTranslationUnit(
+				cidx, ast_files[i],
+				command_line_args, 3,
+				0, 0,
+				CXTranslationUnit_DetailedPreprocessingRecord);
+
+		CXCursor tu_cur = clang_getTranslationUnitCursor(tu_array[i]);
+		clang_visitChildren(tu_cur, visitor_f, &m);
 	}
 }
